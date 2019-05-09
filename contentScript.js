076b9879292a85240a49d73cf5bfcb387d7847ca
Matthew <github@msquare.33mail.com>
2019-05-09 12:22:46 -0400

    Refactor: Comment out contentScript.js Comment out all contentScript.js code, as it is currently unused.

--- contentScript.js
+++ contentScript.js
@@ -1,27 +1,27 @@
-// chrome.runtime.onMessage.addListener(
-//     function (request, sender, sendResponse) {
-//         console.log(sender.tab ?
-//             "from a content script:" + sender.tab.url :
-//             "from the extension");
-//         console.log(request);
-//         // if (request.greeting == "hello")
-//         //     sendResponse({ farewell: "goodbye" });
-//     });
-console.log("Content script loaded");
+// // chrome.runtime.onMessage.addListener(
+// //     function (request, sender, sendResponse) {
+// //         console.log(sender.tab ?
+// //             "from a content script:" + sender.tab.url :
+// //             "from the extension");
+// //         console.log(request);
+// //         // if (request.greeting == "hello")
+// //         //     sendResponse({ farewell: "goodbye" });
+// //     });
+// console.log("Content script loaded");
 
-chrome.runtime.onConnect.addListener(function (port) {
-    console.assert(port.name == "standout");
-    port.onMessage.addListener(function (msg) {
-        if (msg.action == "show") {
-            console.log("Action:show");
-            // alert(msg.action);
-            var port = chrome.runtime.connect({ name: "standout" });
-            var currentDate = new Date();
-            var DateString = currentDate.getFullYear() + ('0' + (currentDate.getMonth() + 1)).slice(-2) + ('0' + currentDate.getDate()).slice(-2);
-            tempJson = {}
-            var data = "Return from Content"
-            tempJson[DateString] = data;
-            port.postMessage(tempJson);
-        }
-    });
-});
+// chrome.runtime.onConnect.addListener(function (port) {
+//     console.assert(port.name == "standout");
+//     port.onMessage.addListener(function (msg) {
+//         if (msg.action == "show") {
+//             console.log("Action:show");
+//             // alert(msg.action);
+//             var port = chrome.runtime.connect({ name: "standout" });
+//             var currentDate = new Date();
+//             var DateString = currentDate.getFullYear() + ('0' + (currentDate.getMonth() + 1)).slice(-2) + ('0' + currentDate.getDate()).slice(-2);
+//             tempJson = {}
+//             var data = "Return from Content"
+//             tempJson[DateString] = data;
+//             port.postMessage(tempJson);
+//         }
+//     });
+// });
