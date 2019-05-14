9e58aab1a711f9340319ae5b63b76cf32815a719
Matthew <github@msquare.33mail.com>
2019-05-14 07:58:26 -0400

    Chore: Add comment linking to uuidv4 src Add comment to document source for uuidv4 function.

--- js/background.js
+++ js/background.js
@@ -6,6 +6,7 @@ var options = {
 var PopupTimeout = 60000 * 5;
 
 function uuidv4() {
+    //Function from https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     )
