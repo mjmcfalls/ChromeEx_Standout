89f6240f60aee3d9c4f07eb4b9cbbaba940826ca
Matthew <github@msquare.33mail.com>
2019-05-20 09:57:30 -0400

    Fix: Rough out nudge based on prior data.

--- js/contentScript_test.js
+++ js/contentScript_test.js
@@ -10,14 +10,16 @@ port.postMessage({ 'request': modalTitle[0].innerHTML });
 port.onMessage.addListener(function (msg) {
     console.log(msg);
     if (msg.data) {
+        console.log("Received data from background.js");
+        console.log(msg);
         var loathed = [];
         var loved = [];
         var notes = [];
-        var LoathedDiv = document.getElementById("loathedacts");
-        console.log("Received data from background.js");
-        console.log(msg);
+        // var LoathedDiv = document.getElementById("loathedacts");
         var skillsNode = document.getElementsByClassName("cirating engagementrating");
         var valueNode = document.getElementsByClassName("cirating valuerating");
+        var textFields = document.getElementsByClassName("input strText");
+
         console.log("Skill avg: " + msg.data['avg']['skillAvg']);
         for (i = 0; i < skillsNode.length; i++) {
             if (msg.data['avg']['skillAvg'] == parseInt(skillsNode[i].getAttribute("data-rating"))) {
@@ -32,33 +34,39 @@ port.onMessage.addListener(function (msg) {
                 valueNode[i].getElementsByClassName.backgroundColor = nudgeColor;
             }
         }
-        // console.log(msg.data);
-        // for (var key in msg.data) {
-        //     console.log(msg.data[key]);
-        //     if (msg.data[key].length > 0) {
-        //         for (i = 0; i < msg.data[key].length; i++) {
-        //             if ("timestamp" in msg.data[key][i]) {
-        //                 // console.log(msg.data[key][i]["timestamp"]);
-        //                 if (msg.data[key][i]["loathed_act"]) {
-        //                     // console.log(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["loathed_act"]);
-        //                     var row = "<tr><td>" + moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + "</td><td></td><td>" + msg.data[key][i]["loathed_act"] + "</td></tr>"
-        //                     LoathedDiv.children[0].innerHTML += LoathedDiv.children[0].innerHTML + row;
-        //                     loathed.push(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["loathed_act"]);
 
-        //                 }
-        //                 if (msg.data[key][i]["loved_act"]) {
-        //                     // console.log(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["loved_act"]);
-        //                     loved.push(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["loathed_act"]);
-        //                 }
-        //                 if (msg.data[key][i]["notes"]) {
-        //                     // console.log(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["notes"]);
-        //                     notes.push(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["loathed_act"]);
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     console.log(loathed);
-        // }
+        for (var key in msg.data) {
+            console.log(msg.data[key]);
+            if (msg.data[key].length > 0) {
+                for (i = 0; i < msg.data[key].length; i++) {
+                    if ("timestamp" in msg.data[key][i]) {
+                        // console.log(msg.data[key][i]["timestamp"]);
+                        if (msg.data[key][i]["loathed_act"]) {
+                            loathed.push(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["loathed_act"]);
+                        }
+                        if (msg.data[key][i]["loved_act"]) {
+                            loved.push(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["loathed_act"]);
+                        }
+                        if (msg.data[key][i]["notes"]) {
+                            notes.push(moment.unix(msg.data[key][i]["timestamp"]).format("YYYY-MM-DD") + " - " + msg.data[key][i]["loathed_act"]);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        for (i = 0; i < textFields.length; i++) {
+            if (textFields[i].getAttribute("data-gaaction") == "love") {
+                console.log("loathed txt");
+                console.log(loathed);
+            }
+            else if (textFields[i].getAttribute("data-gaaction") == "loathe") {
+                console.log("loved txt");
+                console.log(loved);
+            }
+        }
+
 
         // alert(msg.action);
         // var port = chrome.runtime.connect({ name: "standout" });
